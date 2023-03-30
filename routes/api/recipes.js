1b6a4432aee89a50894eb8f9e0a3a959fb8aa3ec
efirnet <efirnet@gmail.com>
2023-03-30 17:56:25 +0300

    close  authenticate

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -4,9 +4,9 @@ const { ctrlWrapper } = require("../../helpers");
 const { authenticate } = require("../../middleware");
 const router = express.Router();
 
-router.get('/category-list', authenticate, ctrlWrapper(ctrl.getCategoryList));
-router.get('/main-page', authenticate, ctrlWrapper(ctrl.getMainPage));
-router.get('/:category', authenticate, ctrlWrapper(ctrl.getRecipesByCategory));
-router.get('/id/:id', authenticate, ctrlWrapper(ctrl.getRecipeById));
-router.post('/search', authenticate, ctrlWrapper(ctrl.searchRecipe))
+router.get('/category-list', /* authenticate, */ ctrlWrapper(ctrl.getCategoryList));
+router.get('/main-page', /* authenticate, */ ctrlWrapper(ctrl.getMainPage));
+router.get('/:category', /* authenticate, */ ctrlWrapper(ctrl.getRecipesByCategory));
+router.get('/id/:id', /* authenticate, */ ctrlWrapper(ctrl.getRecipeById));
+router.post('/search', /* authenticate, */ ctrlWrapper(ctrl.searchRecipe))
 module.exports = router;
