396aaa2f523761a18d53fff93dd119733e530c7b
efirnet <efirnet@gmail.com>
2023-03-30 17:15:39 +0300

    add upload cloud

--- models/user.js
+++ models/user.js
@@ -18,6 +18,9 @@ const userSchema = new Schema({
     type: String,
     default: null,
   },
+  avatarURL: {
+    type: String,
+  },
 }, { versionKey: false, timestamps: true });
 
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const ctrl = require("../../controllers/auth");
 const { ctrlWrapper } = require("../../helpers");
-const { validateBody, authenticate } = require("../../middleware");
+const { validateBody, authenticate, uploadCloud } = require("../../middleware");
 const { signupSchema, signinSchema } = require('../../schemas/user');
 
 const router = express.Router();
@@ -15,5 +15,7 @@ router.get("/logout", authenticate, ctrlWrapper(ctrl.logout));
 router.get("/:userId", authenticate, ctrlWrapper(ctrl.getById))
 
 router.put("/:userId", authenticate, validateBody(signupSchema), ctrlWrapper(ctrl.updateById))
+
+router.post("/", authenticate, uploadCloud.single('avatar'), ctrlWrapper(ctrl.petRegister));
 
 module.exports = router;
