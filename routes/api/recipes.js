17728d513f935185139684f755f74299d5ba4156
efirnet <efirnet@gmail.com>
2023-03-31 18:31:48 +0300

    close authenticate

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -8,8 +8,8 @@ router.get('/category-list', /* authenticate, */ ctrlWrapper(ctrl.getCategoryLis
 router.get('/main-page', /* authenticate, */ ctrlWrapper(ctrl.getMainPage));
 router.get('/:category', /* authenticate, */ ctrlWrapper(ctrl.getRecipesByCategory));
 router.get('/id/:id', /* authenticate, */ ctrlWrapper(ctrl.getRecipeById));
-router.get('/', authenticate, ctrlWrapper(ctrl.getUserRecipe))
+router.get('/', /* authenticate, */ ctrlWrapper(ctrl.getUserRecipe))
 router.post('/search', /* authenticate, */ ctrlWrapper(ctrl.searchRecipe))
-router.post('/add', authenticate, ctrlWrapper(ctrl.addRecipe))
-router.delete('/remove/:id', authenticate, ctrlWrapper(ctrl.removeRecipe))
+router.post('/add', /* authenticate, */ ctrlWrapper(ctrl.addRecipe))
+router.delete('/remove/:id', /* authenticate, */ ctrlWrapper(ctrl.removeRecipe))
 module.exports = router;
