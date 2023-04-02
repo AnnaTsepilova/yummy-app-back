304739bd4cf4f248241929ef1d8fa2cf66601f8b
QNeee <vovasagan7@gmail.com>
2023-04-02 13:29:24 +0300

    done

--- controllers/recipes/recipesByCategory.js
+++ controllers/recipes/recipesByCategory.js
@@ -3,7 +3,8 @@ const Recipe = require("../../models/recipe");
 const getRecipesByCategory = async (req, res) => {
     const limitNumber = 8;
     const { category } = req.params;
-    const recipes = await Recipe.find({ category }).select({ title: 1, preview: 1 }).limit(limitNumber);
+    const categoryRequest = category.charAt(0).toUpperCase() + category.slice(1).toLowerCase();
+    const recipes = await Recipe.find({ category: categoryRequest }).select({ title: 1, preview: 1 }).limit(limitNumber);
     return res.status(200).json(recipes);
 }
 module.exports = getRecipesByCategory;
