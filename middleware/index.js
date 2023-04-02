1b53eb53b1c242cfa8b620030e2c46e8b783bf0b
efirnet <efirnet@gmail.com>
2023-04-02 11:30:17 +0300

    fix bug

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,9 @@
 const validateBody = require('./validateBody');
 const authenticate = require('./authenticate');
 const imageUploader = require('./imageUploader');
-const errorHandler = require('./errorHandler');
 
 module.exports = {
   validateBody,
   authenticate,
-  imageUploader,
-  errorHandler
+  imageUploader
 };
