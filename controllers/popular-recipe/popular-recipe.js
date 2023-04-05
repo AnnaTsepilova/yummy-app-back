bf4bee311035921aabbf85c842935a504e34569d
QNeee <vovasagan7@gmail.com>
2023-04-05 16:33:28 +0300

    end

--- controllers/popular-recipe/popular-recipe.js
+++ controllers/popular-recipe/popular-recipe.js
@@ -3,13 +3,14 @@ const Recipe = require("../../models/recipe");
 const { NotFound } = require("http-errors");
 
 const getPopularRecipe = async (req, res) => {
-  const recipes = await Recipe.find()
+  const recipesSorted = await Recipe.find()
     .limit(4)
     .sort({ favorites: -1 });
 
-  if (!recipes) {
+  if (!recipesSorted) {
     throw new NotFound(`recipes not found`);
   }
+  const recipes = recipesSorted.sort((a, b) => b.favorites.length - a.favorites.length);
   return res.status(200).json(recipes);
 };
 
