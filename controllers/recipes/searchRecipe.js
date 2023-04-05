45873fa624bbb9a8a2e044a346b44e86c83553a2
QNeee <vovasagan7@gmail.com>
2023-04-05 21:25:29 +0300

    end

--- controllers/recipes/searchRecipe.js
+++ controllers/recipes/searchRecipe.js
@@ -1,11 +1,7 @@
-const { BadRequest } = require('http-errors');
+const { BadRequest, NotFound } = require('http-errors');
 const Ingredients = require('../../models/ingredient');
 const Recipe = require('../../models/recipe');
 const searchRecipe = async (req, res) => {
-    const { value, flag } = req.body;
-    if (!value && !flag) {
-        throw new BadRequest('missing fields , need {value,flag}')
-    }
     const limitNumber = 12;
     let {
         page = 1,
@@ -14,16 +10,16 @@ const searchRecipe = async (req, res) => {
     } = req.query;
     limit = parseInt(limit) > limitNumber ? limitNumber : parseInt(limit);
     skip = parseInt(page) === 1 ? 0 : parseInt(page) * limit - limitNumber;
-    if (flag === 'title') {
-        const pagination = await Recipe.find({ title: { $regex: new RegExp(`${value}`, 'i') } }).select({ title: 1, preview: 1 });
-        const finded = await Recipe.find({ title: { $regex: new RegExp(`${value}`, 'i') } }).select({ title: 1, preview: 1 }).skip(skip).limit(limitNumber);
+    if (req.query.title) {
+        const pagination = await Recipe.find({ title: { $regex: req.query.title, $options: "i" } }).select({ title: 1, preview: 1 });
+        const finded = await Recipe.find({ title: { $regex: req.query.title, $options: "i" } }).select({ title: 1, preview: 1 }).skip(skip).limit(limitNumber);
         const totalHits = pagination.length;
         const hits = finded.length;
         return res.status(200).json({ finded, totalHits, hits });
     }
-    const ingredient = await Ingredients.findOne({ ttl: { $regex: new RegExp(`${value}`, 'i') } }).select({ _id: 1 });
+    const ingredient = await Ingredients.findOne({ ttl: { $regex: req.query.ingredient, $options: "i" } }).select({ _id: 1 });
     if (!ingredient) {
-        throw new NotFound(`ingridient ${value} not found`);
+        throw new NotFound(`ingridient ${req.query.ingredient} not found`);
     }
     const pagination = await Recipe.find({
         ingredients: { $elemMatch: { id: ingredient._id } },
