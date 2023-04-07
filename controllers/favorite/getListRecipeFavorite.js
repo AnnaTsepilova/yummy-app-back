74997c894e4ba626bf8eea0cb77be842b7381662
QNeee <vovasagan7@gmail.com>
2023-04-07 16:40:38 +0300

    end

--- controllers/favorite/getListRecipeFavorite.js
+++ controllers/favorite/getListRecipeFavorite.js
@@ -2,10 +2,21 @@ const Recipe = require("../../models/recipe");
 const { NotFound } = require("http-errors");
 
 const getListRecipeFavorite = async (req, res) => {
-  const recipeList = await Recipe.find({ favorites: req.user._id });
-  if (!recipeList) {
+  const limitNumber = 4;
+  let {
+    page = 1,
+    limit = limitNumber,
+    skip = 0,
+  } = req.query;
+  limit = parseInt(limit) > limitNumber ? limitNumber : parseInt(limit);
+  skip = parseInt(page) === 1 ? 0 : parseInt(page) * limit - limitNumber;
+  const pagination = await Recipe.find({ favorites: req.user._id });
+  if (!pagination) {
     throw new NotFound(`ingridients not found`);
   }
-  return res.status(200).json(recipeList);
+  const totalHits = pagination.length;
+  const results = await Recipe.find({ favorites: req.user._id }).skip(skip).limit(limit)
+  const hits = results.length;
+  return res.status(200).json({ results, totalHits, hits });
 };
 module.exports = getListRecipeFavorite;
--- controllers/recipes/ownRecipes.js
+++ controllers/recipes/ownRecipes.js
@@ -44,8 +44,19 @@ const removeRecipe = async (req, res) => {
 }
 const getUserRecipe = async (req, res) => {
     const { id } = req.user;
-    const userRecipes = await Recipe.find({ owner: id })
-    return res.status(200).json(userRecipes);
+    const limitNumber = 4;
+    let {
+        page = 1,
+        limit = limitNumber,
+        skip = 0,
+    } = req.query;
+    limit = parseInt(limit) > limitNumber ? limitNumber : parseInt(limit);
+    skip = parseInt(page) === 1 ? 0 : parseInt(page) * limit - limitNumber;
+    const pagination = await Recipe.find({ owner: id })
+    const totalHits = pagination.length;
+    const results = await Recipe.find({ owner: id }).skip(skip).limit(limit)
+    const hits = results.length;
+    return res.status(200).json({ results, totalHits, hits });
 }
 
 module.exports = {
