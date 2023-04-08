8db391962a96133d3c88d068e79460b091ca5960
QNeee <vovasagan7@gmail.com>
2023-04-08 14:49:39 +0300

    end

--- controllers/shopping-list/addInShopping-list.js
+++ controllers/shopping-list/addInShopping-list.js
@@ -6,6 +6,7 @@ const addItemInList = async (req, res) => {
     if (!req.body) {
         throw new BadRequest('Need Body');
     }
+    console.log(req.body);
     const ingrName = Object.keys(req.body).join('');
     const ingrCount = Object.values(req.body).join('');
     const user = await User.findById(id);
@@ -22,25 +23,6 @@ const addItemInList = async (req, res) => {
         id: ingrId._id,
         recipesId,
         measure: ingrCount
-    }
-    const messureName = ingrCount.split(' ')[1];
-    const checkIds = user.shopingList.filter(item => item.id.toString() === ingrId._id.toString())
-    const messure = user.shopingList.filter(item => item.id.toString() === ingrId._id.toString()).filter(item => item.measure?.split(' ')[1] === messureName).map(item => item.measure);
-    if (checkIds.length > 0 && messure.length > 0) {
-        const messureCount = parseInt(ingrCount.split(' ')[0]);
-        const messureItem = messure.join('').split(' ');
-        const ingrToShoppingList = {
-            ttl: ingr.ttl,
-            thb: ingr.thb,
-            id: ingrId._id,
-            recipesId,
-            measure: !messureItem[1] ? parseInt(messureItem[0]) + messureCount + '' : parseInt(messureItem[0]) + messureCount + ' ' + messureItem[1]
-
-        }
-        const newIngrCount = await user.shopingList.findIndex(item => item.id.toString() === ingrToShoppingList.id.toString() && item.measure?.split(' ')[1] === messureName);
-        await user.shopingList.splice(newIngrCount, 1, ingrToShoppingList);
-        const addedToShoppingList = await User.findOneAndUpdate({ _id: id }, { shopingList: user.shopingList }, { new: true }).select({ shopingList: 1 })
-        return res.status(200).json(addedToShoppingList);
     }
     const addedToShoppingList = await User.findOneAndUpdate({ _id: id }, { $push: { shopingList: ingrToShoppingList } }, { new: true }).select({ shopingList: 1 })
     return res.status(200).json(addedToShoppingList);
--- controllers/shopping-list/removeFromShopping-list.js
+++ controllers/shopping-list/removeFromShopping-list.js
@@ -6,27 +6,19 @@ const removeItemFromList = async (req, res) => {
     const { shoppingListId } = req.params;
     const { recipeId } = req.query;
     const user = await User.findById(id);
-    const newArr = []
     if (recipeId) {
         const recipesId = await user.shopingList.filter(item => item.id.toString() === shoppingListId.toString()).flatMap(item => item.recipesId)
         const recipesIdIndex = await recipesId.findIndex(item => item === recipeId);
         if (recipesIdIndex === -1) {
             throw new NotFound(`id ${shoppingListId} not found`);
         }
-        await recipesId.splice(recipesIdIndex, 1);
-        const shoppingList = await user.shopingList.filter(item => item.id.toString() === shoppingListId.toString()).flatMap(item => item);
-        for (let i = 0; i < shoppingList.length; i++) {
-            const ingrToShoppingList = {
-                ttl: shoppingList[i].ttl,
-                thb: shoppingList[i].thb,
-                id: shoppingList[i].id,
-                recipesId,
-                measure: shoppingList[i].measure.split(' ')[0] ? parseInt(shoppingList[i].measure.split('')[0]) - parseInt(req.query.measure.split(' ')[0]) + " " + shoppingList[i].measure.split(' ')[1] : parseInt(shoppingList[i].measure.split('')[0]) - parseInt(req.query.measure.split(' ')[0]) + ''
-            }
-            newArr.push(ingrToShoppingList);
-        }
-        await User.findOneAndUpdate({ _id: id }, { shopingList: newArr }, { new: true })
+        const shoppingList = await user.shopingList.filter(item => item.recipesId.includes(recipeId));
+        const shopingListIndex = await shoppingList.findIndex(item => item.measure === req.query.measure);
+        await shoppingList.splice(shopingListIndex, 1)
+        await User.findOneAndUpdate({ _id: id }, { shopingList: shoppingList }, { new: true })
         return res.status(200).json({ message: "success" });
+
+
     }
     const findShopingListIndex = await user.shopingList.findIndex(item => item.id.toString() === shoppingListId.toString() && item.measure === req.query.measure);
     if (findShopingListIndex === -1) {
@@ -36,5 +28,4 @@ const removeItemFromList = async (req, res) => {
     await User.findOneAndUpdate({ _id: id }, { shopingList: user.shopingList }, { new: true })
     return res.status(200).json({ message: "success" });
 }
-
 module.exports = removeItemFromList;
