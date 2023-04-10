8bed62b88a5f8e8b4b67c2d5502f8b7d98b088d4
QNeee <vovasagan7@gmail.com>
2023-04-10 11:25:31 +0300

    end

--- controllers/recipes/categoryList.js
+++ controllers/recipes/categoryList.js
@@ -1,7 +1,7 @@
 const getCategoryList = async (req, res) => {
     const category = ['Beef', 'Breakfast', 'Chicken', 'Dessert', 'Goat', 'Lamb',
         'Miscellaneous', 'Pasta', 'Pork', 'Seafood', 'Side', 'Starter',
-        'Vegan', 'Vegeterian']
+        'Vegan', 'Vegetarian']
     return res.status(200).json(category);
 }
 module.exports = getCategoryList;
