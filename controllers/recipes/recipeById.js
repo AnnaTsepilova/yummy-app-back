3f8ef6a152e0cf942b859c2c1f85cd4e4cb04f9b
QNeee <vovasagan7@gmail.com>
2023-04-10 21:29:06 +0300

    end

--- controllers/recipes/recipeById.js
+++ controllers/recipes/recipeById.js
@@ -3,22 +3,22 @@ const { NotFound } = require('http-errors');
 const Ingredients = require("../../models/ingredient");
 const getRecipeById = async (req, res) => {
     const { id } = req.params;
-    const recipeFromDb = await Recipe.findOne({ _id: id });
-    const newIngredients = [];
-    const ingrs = recipeFromDb.ingredients;
-    for (let i = 0; i < ingrs.length; i++) {
-        const ingrInfo = await Ingredients.findOne({ _id: ingrs[i].id });
-        const ingr = {
-            _id: ingrInfo._id,
-            ttl: ingrInfo.ttl,
-            thb: ingrInfo.thb,
-            measure: recipeFromDb.ingredients[i].measure
-        }
-        newIngredients.push(ingr);
-    }
+    const recipeFromDb = await Recipe.findOne({ _id: id }).select('title category area instructions description preview time favorites likes createdAt updatedAt ingredients').lean();
+
     if (!recipeFromDb) {
         throw new NotFound(`recipe with id ${id} not found`)
     }
+
+    const ingredientIds = recipeFromDb.ingredients.map(ingr => ingr.id);
+    const ingredients = await Ingredients.find({ _id: { $in: ingredientIds } }).select('_id ttl thb').lean();
+
+    const newIngredients = recipeFromDb.ingredients.map((ingr, i) => ({
+        _id: ingredients[i]._id,
+        ttl: ingredients[i].ttl,
+        thb: ingredients[i].thb,
+        measure: ingr.measure
+    }));
+
     const recipe = {
         _id: recipeFromDb._id,
         title: recipeFromDb.title,
@@ -33,8 +33,8 @@ const getRecipeById = async (req, res) => {
         ingredients: newIngredients,
         createdAt: recipeFromDb.createdAt,
         updatedAt: recipeFromDb.updatedAt
-
     }
+
     return res.status(200).json(recipe);
 }
 
