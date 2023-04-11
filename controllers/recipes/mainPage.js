564252c4d3ab214b807445c4a8eeeecd5a374a3f
QNeee <vovasagan7@gmail.com>
2023-04-11 12:33:28 +0300

    end

--- controllers/recipes/mainPage.js
+++ controllers/recipes/mainPage.js
@@ -4,13 +4,15 @@ const Recipe = require("../../models/recipe");
 const getMainPage = async (req, res) => {
     const limitNumber = 4;
     const category = ['Breakfast', 'Miscellaneous', 'Vegan', 'Dessert'];
-    let result = {};
-    for (let i = 0; i < category.length; i++) {
-        const recipes = await Recipe.find({ category: category[i] }).select({ title: 1, preview: 1 }).limit(limitNumber);
-        result[category[i]] = recipes;
-    }
-    return res.status(200).json(result);
+    const promises = category.map(cat => Recipe.find({ category: cat }).select({ title: 1, preview: 1 }).limit(limitNumber));
+    const result = await Promise.all(promises);
+    const data = category.reduce((acc, cat, index) => {
+        acc[cat] = result[index];
+        return acc;
+    }, {});
+    return res.status(200).json(data);
 }
+
 
 
 module.exports = getMainPage;
